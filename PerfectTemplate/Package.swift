6479ba9c24997024d1b640d2832c4f30117f3c7f
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2022-01-03 13:36:36 +0100

    Update code

--- PerfectTemplate/Package.swift
+++ PerfectTemplate/Package.swift
@@ -3,6 +3,24 @@
 import PackageDescription
 
 let package = Package(
+  name: "MyAwesomeProject",
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .target(
+      name: "MyAwesomeProject",
+      dependencies: ["PerfectHTTPServer"]
+    )
+  ]
+)
+
+
+/*
+let package = Package(
 	name: "PerfectTemplate",
 	products: [
 		.executable(name: "PerfectTemplate", targets: ["PerfectTemplate"])
@@ -14,3 +32,4 @@ let package = Package(
 		.target(name: "PerfectTemplate", dependencies: ["PerfectHTTPServer"])
 	]
 )
+*/
--- PerfectTemplate/Sources/PerfectTemplate/main.swift
+++ PerfectTemplate/Sources/PerfectTemplate/main.swift
@@ -20,38 +20,19 @@
 import PerfectHTTP
 import PerfectHTTPServer
 
-// An example request handler.
-// This 'handler' function can be referenced directly in the configuration below.
-func handler(request: HTTPRequest, response: HTTPResponse) {
-  // Respond with a simple message.
+// Register your own routes and handlers
+var routes = Routes()
+routes.add(method: .get, uri: "/") {
+  request, response in
   response.setHeader(.contentType, value: "text/html")
   response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
-  // Ensure that response.completed() is called when your processing is done.
-  response.completed()
+    .completed()
 }
 
-// Configure one server which:
-//	* Serves the hello world message at <host>:<port>/
-//	* Serves static files out of the "./webroot"
-//		directory (which must be located in the current working directory).
-//	* Performs content compression on outgoing data when appropriate.
-var routes = Routes()
-routes.add(method: .get, uri: "/", handler: handler)
-
-routes.add(
-  method: .get, uri: "/**",
-  handler: StaticFileHandler(
-    documentRoot: "./webroot",
-    allowResponseFilters: true
-  ).handleRequest
-)
-
-try HTTPServer.launch(
-  name: "localhost",
-  port: 8181,
-  routes: routes,
-  responseFilters: [
-    (PerfectHTTPServer.HTTPFilter.contentCompression(data: [:]), HTTPFilterPriority.high)
-  ]
-)
-
+do {
+  // Launch the HTTP server.
+  try HTTPServer.launch(
+    .server(name: "localhost", port: 8182, routes: routes))
+} catch {
+  fatalError("\(error)") // fatal error launching one of the servers
+}
