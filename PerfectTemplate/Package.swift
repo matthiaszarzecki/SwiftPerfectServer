6b759a1086d4ce378293525769f05892dedda8f1
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2022-01-03 11:54:17 +0100

    re-add fresh project

--- PerfectTemplate/Package.swift
+++ PerfectTemplate/Package.swift
@@ -3,14 +3,14 @@
 import PackageDescription
 
 let package = Package(
-  name: "PerfectTemplate",
-  products: [
-    .executable(name: "PerfectTemplate", targets: ["PerfectTemplate"])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
-  ],
-  targets: [
-    .target(name: "PerfectTemplate", dependencies: ["PerfectHTTPServer"])
-  ]
+	name: "PerfectTemplate",
+	products: [
+		.executable(name: "PerfectTemplate", targets: ["PerfectTemplate"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+	],
+	targets: [
+		.target(name: "PerfectTemplate", dependencies: ["PerfectHTTPServer"])
+	]
 )
--- PerfectTemplate/Sources/PerfectTemplate/main.swift
+++ PerfectTemplate/Sources/PerfectTemplate/main.swift
@@ -23,18 +23,11 @@ import PerfectHTTPServer
 // An example request handler.
 // This 'handler' function can be referenced directly in the configuration below.
 func handler(request: HTTPRequest, response: HTTPResponse) {
-  // Respond with a simple message.
-  response.setHeader(
-    .contentType,
-    value: "text/html"
-  )
-
-  response.appendBody(
-    string: "<html><title>Howdy, world!</title><body>Howdy, world!</body></html>"
-  )
-
-  // Ensure that response.completed() is called when your processing is done.
-  response.completed()
+	// Respond with a simple message.
+	response.setHeader(.contentType, value: "text/html")
+	response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+	// Ensure that response.completed() is called when your processing is done.
+	response.completed()
 }
 
 // Configure one server which:
@@ -43,24 +36,12 @@ func handler(request: HTTPRequest, response: HTTPResponse) {
 //		directory (which must be located in the current working directory).
 //	* Performs content compression on outgoing data when appropriate.
 var routes = Routes()
-
-routes.add(
-  method: .get,
-  uri: "/", handler: handler
-)
-
-routes.add(
-  method: .get,
-  uri: "/**",
-  handler: StaticFileHandler(documentRoot: "./webroot", allowResponseFilters: true).handleRequest
-)
-
-try HTTPServer.launch(
-  name: "localhost",
-  port: 8181,
-  routes: routes,
-  responseFilters: [
-    (PerfectHTTPServer.HTTPFilter.contentCompression(data: [:]), HTTPFilterPriority.high)
-  ]
-)
+routes.add(method: .get, uri: "/", handler: handler)
+routes.add(method: .get, uri: "/**",
+		   handler: StaticFileHandler(documentRoot: "./webroot", allowResponseFilters: true).handleRequest)
+try HTTPServer.launch(name: "localhost",
+					  port: 8181,
+					  routes: routes,
+					  responseFilters: [
+						(PerfectHTTPServer.HTTPFilter.contentCompression(data: [:]), HTTPFilterPriority.high)])
 
