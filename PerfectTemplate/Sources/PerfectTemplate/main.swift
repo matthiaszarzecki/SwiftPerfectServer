7eab5fe07045f8221ddd0e21374b4a1401585b93
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2022-01-03 12:28:27 +0100

    Update gitignore, format files

--- PerfectTemplate/Sources/PerfectTemplate/main.swift
+++ PerfectTemplate/Sources/PerfectTemplate/main.swift
@@ -23,11 +23,11 @@ import PerfectHTTPServer
 // An example request handler.
 // This 'handler' function can be referenced directly in the configuration below.
 func handler(request: HTTPRequest, response: HTTPResponse) {
-	// Respond with a simple message.
-	response.setHeader(.contentType, value: "text/html")
-	response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
-	// Ensure that response.completed() is called when your processing is done.
-	response.completed()
+  // Respond with a simple message.
+  response.setHeader(.contentType, value: "text/html")
+  response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+  // Ensure that response.completed() is called when your processing is done.
+  response.completed()
 }
 
 // Configure one server which:
@@ -37,11 +37,20 @@ func handler(request: HTTPRequest, response: HTTPResponse) {
 //	* Performs content compression on outgoing data when appropriate.
 var routes = Routes()
 routes.add(method: .get, uri: "/", handler: handler)
-routes.add(method: .get, uri: "/**",
-		   handler: StaticFileHandler(documentRoot: "./webroot", allowResponseFilters: true).handleRequest)
-try HTTPServer.launch(name: "localhost",
-					  port: 8181,
-					  routes: routes,
-					  responseFilters: [
-						(PerfectHTTPServer.HTTPFilter.contentCompression(data: [:]), HTTPFilterPriority.high)])
+
+routes.add(
+  method: .get, uri: "/**",
+  handler: StaticFileHandler(
+    documentRoot: "./webroot",
+    allowResponseFilters: true
+  ).handleRequest
+)
+
+try HTTPServer.launch(
+  name: "localhost",
+  port: 8181,
+  routes: routes,
+  responseFilters: [
+    (PerfectHTTPServer.HTTPFilter.contentCompression(data: [:]), HTTPFilterPriority.high)]
+)
 
