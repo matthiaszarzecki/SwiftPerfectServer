f53d145bad6b0ed58dd15e429c03dbcf207e7d7c
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2022-01-04 08:41:38 +0100

    Extract routes, ignore ds store

--- PerfectTemplate/Sources/PerfectTemplate/main.swift
+++ PerfectTemplate/Sources/PerfectTemplate/main.swift
@@ -2,87 +2,97 @@
 //  main.swift
 //  PerfectTemplate
 //
-//  Created by Kyle Jessup on 2015-11-05.
-//	Copyright (C) 2015 PerfectlySoft, Inc.
-//
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Perfect.org open source project
-//
-// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
-// Licensed under Apache License v2.0
-//
-// See http://perfect.org/licensing.html for license information
-//
-//===----------------------------------------------------------------------===//
+//  Created by Matthias Zarzecki on 13.12.21.
 //
 
 import PerfectHTTP
 import PerfectHTTPServer
 
-// Register your own routes and handlers
-var routes = Routes()
-
 /// Sets the basic URI that is seen without parameters,
 /// in this case a html page.
-routes.add(method: .get, uri: "/") { request, response in
-  response.setHeader(.contentType, value: "text/html")
-  response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
-    .completed()
+private func addBasicRoute() {
+  routes.add(method: .get, uri: "/") { request, response in
+    response.setHeader(.contentType, value: "text/html")
+    response.appendBody(
+      string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>"
+    )
+      .completed()
+  }
 }
 
 /// Sets an html page on a different URI.
-routes.add(method: .get, uri: "/test") { request, response in
-  response.setHeader(.contentType, value: "text/html")
-  response.appendBody(string: "<html><title>Hello, world 2!</title><body>Hello, world 2!</body></html>")
-    .completed()
+private func addSecondScreenRoute() {
+  routes.add(method: .get, uri: "/test") { request, response in
+    response.setHeader(.contentType, value: "text/html")
+    response.appendBody(
+      string: "<html><title>Hello, world 2!</title><body>Hello, world 2!</body></html>"
+    )
+      .completed()
+  }
 }
 
 /// Returns JSON data.
-routes.add(method: .get, uri: "/json") { request, response in
-  response.setHeader(.contentType, value: "application/json")
-  let jsonResponse: [String: Any] = [
-    "isServeAlive": true,
-    "a": 1,
-    "b": 0.1,
-    "c": true,
-    "d": [2, 4, 5, 7, 8]
-  ]
+private func addJsonRoute() {
+  routes.add(method: .get, uri: "/json") { request, response in
+    response.setHeader(.contentType, value: "application/json")
+    let jsonResponse: [String: Any] = [
+      "isServeAlive": true,
+      "a": 1,
+      "b": 0.1,
+      "c": true,
+      "d": [2, 4, 5, 7, 8]
+    ]
 
-  do {
-    try response.setBody(json: jsonResponse)
-  } catch {
+    do {
+      try response.setBody(json: jsonResponse)
+    } catch {
+      print("\(error)")
+    }
+    response.completed()
   }
-  response.completed()
 }
 
 /// Post Method which takes the data from the URI and
 /// adds it together. Cannot be run from browser.
 /// Example Call: "http://localhost:8182/add?operandOne=30&operandTwo=23"
-routes.add(method: .post, uri: "/add") { request, response in
-  response.setHeader(.contentType, value: "application/json")
+private func addPostRoute() {
+  routes.add(method: .post, uri: "/add") { request, response in
+    response.setHeader(.contentType, value: "application/json")
 
-  var responsePayload: [String: Any] = ["sum": 0]
-  if let operandOne = request.param(name: "operandOne"),
-     let operandTwo = request.param(name: "operandTwo") {
-    if let leftSide = Int(operandOne),
-       let rightSide = Int(operandTwo) {
-      responsePayload["sum"] = leftSide + rightSide
+    // Set basic response payload
+    var responsePayload: [String: Any] = ["sum": 0]
+
+    // Get parameters from request, parse them, and calculate them
+    if let operandOne = request.param(name: "operandOne"),
+       let operandTwo = request.param(name: "operandTwo") {
+      if let leftSide = Int(operandOne),
+         let rightSide = Int(operandTwo) {
+        responsePayload["sum"] = leftSide + rightSide
+      }
     }
-  }
 
+    do {
+      try response.setBody(json: responsePayload)
+    } catch {
+      print("\(error)")
+    }
+    response.completed()
+  }
+}
+
+private func launchServer(on port: Int, with routes: Routes) {
   do {
-    try response.setBody(json: responsePayload)
+    try HTTPServer.launch(
+      .server(name: "localhost", port: port, routes: routes)
+    )
   } catch {
+    fatalError("\(error)")
   }
-  response.completed()
 }
 
-// Launch the HTTP server.
-do {
-  try HTTPServer.launch(
-    .server(name: "localhost", port: 8182, routes: routes)
-  )
-} catch {
-  fatalError("\(error)") // fatal error launching one of the servers
-}
+var routes = Routes()
+addBasicRoute()
+addSecondScreenRoute()
+addJsonRoute()
+addPostRoute()
+launchServer(on: 8182, with: routes)
